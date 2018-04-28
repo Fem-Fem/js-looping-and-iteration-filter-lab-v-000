32e52e36af52736658dcc13253d5360fed4d7af1
Obafemi Fadairo <ofpupp@princeton.edu>
2018-04-28 23:10:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ function findMatching(name) {
   return name
   driver.filter(function (driver_name) { return driver_name == name; });
 }
+
+// function fuzzyMatch
+// fuzzyMatch - This function takes an array of drivers and a string as arguments for querying the array, and returns all drivers whose names begin with the provided letters.
