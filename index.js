7ae96e211084c9bc82b98dbd09c28a84b7d6b304
Obafemi Fadairo <ofpupp@princeton.edu>
2018-04-28 23:20:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function findMatching(drivers, name) {
 
 function fuzzyMatch(drivers, string) {
   length = string.length;
-  return string
+  // return string
   x = drivers.filter(function (driver_name) { if (driver_name[0, length] == string) {return driver_name}; });
   return x
 }
